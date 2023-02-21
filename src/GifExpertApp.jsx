f1c678554828be3d0694ca315c2b166b0d653142
Martin <martinfort13@gmail.com>
2023-02-21 15:53:09 -0300

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState([ 'One Punch' ]);
+    
+    const onAddCategory= ( newCategory ) => {
+        // categories.push('Valorant');
+        // setCategories( cat =>[ ...cat, 'Valorant' ] );
+        // console.log(newCategory);
+
+        if( categories.includes(newCategory)  ) return;
+
+        setCategories( [  newCategory , ...categories ] );
+
+    }
+  return (
+    <>
+
+    <h1>GifExpertApp</h1>
+
+    <AddCategory 
+    onNewCategory={ event =>onAddCategory(event) }
+    />
+
+    {
+        categories.map( ( category ) =>(
+            <GifGrid 
+                key={ category } 
+                category={ category }/>
+            ))
+    }
+
+    
+    </>
+    )
+}
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
+/* 
+se puede usar control + espacio para buscar la direccion 
+si es que no aparece mientras la escribimos
+ */
+
